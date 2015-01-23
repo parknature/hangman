7d9d69ba976871d2111e7a5f65fb912c6e88d999
parknature <parknature1013@hanmanil.net>
2015-01-23 14:26:39 +0900

    changed

--- test.py
+++ test.py
@@ -5,11 +5,11 @@ import unittest
 
 class hangmanTestCase(unittest.TestCase):
     def test_checkCorrectAnswer(self):
-        ret = hangmantest.checkCorrectAnswer('tac', 'cat')
+        ret = hangman.checkCorrectAnswer('tac', 'cat')
         self.assertTrue(ret)
 
     def test_checkWrongAnswer(self):
-        ret = hangmantest.checkCorrectAnswer('ta', 'cat')
+        ret = hangman.checkCorrectAnswer('ta', 'cat')
         self.assertEqual(ret, False)
 
 
